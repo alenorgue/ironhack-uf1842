ae90d4b711bfae564201d8be29148ad532fd4a4e
alenorgue <alenoriegag@gmail.com>
2025-04-01 10:29:22 +0200

    finished Dom and bonus

--- dom/app.js
+++ dom/app.js
@@ -1,1 +1,18 @@
 // ¿En tu páis de origen es normal dejar propina?
+const btnCalTotal = document.getElementById('calculate');
+const billInput = document.getElementById('bill-amount');
+const tipPercentageInput = document.getElementById('percentage-tip');
+const outputTotal = document.getElementById('total');
+const tipAmount = document.getElementById('tip-amount')
+
+function calculateTotal(){
+    const billValue = Number(billInput.value);
+    const tipValue = Number(tipPercentageInput.value);
+    const tipAmountValue = billValue * (tipValue / 100);
+    const totalValue = Number(billValue) + Number(tipAmountValue);
+    tipAmount.value = tipAmountValue.toFixed(2);
+    outputTotal.value = totalValue.toFixed(2);
+    console.log(totalValue)
+}
+
+btnCalTotal.addEventListener('click', calculateTotal)
